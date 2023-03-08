d0d8704f41cd95cf371d9b04e27911e7633e0ca6
Andrii <lutsiukandrey1@gmail.com>
2023-03-08 16:45:29 +0200

    start

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,31 +1,49 @@
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { Component } from 'react';
 
-import { Component } from "react"
 export class ContactList extends Component {
-    
-    onDellateBtn= event => {
-        this.props.delateContact(event.currentTarget.id)
-     
-
+  onDellateBtn = event => {
+    this.props.delateContact(event.currentTarget.id);
+  };
+  render() {
+    if (this.props.data.length > 0) {
+      return (
+        <ul>
+          {this.props.data.map(({ id, name, number }) => {
+            return (
+              <ContactItem key={id}>
+                {name} : {number}
+                <ContactBtn id={id} type="button" onClick={this.onDellateBtn}>
+                  delate
+                </ContactBtn>
+              </ContactItem>
+            );
+          })}
+        </ul>
+      );
     }
-    render(){
+  }
+}
 
-        
-    if(this.props.data.length >0){
-        return(
-   <ul>
-    {this.props.data.map(({id,name,number})=>{
-        return (
-            <li key={id}>{name} : {number}
-            <button id={id} type="button" onClick={this.onDellateBtn}>delate</button></li>
-        )
-    })}
-   </ul>
-        
-        
-        
-        
-        )
-    }
-    
-}
-}
+ContactList.propTypes = {
+  delateContact: PropTypes.func,
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ),
+};
+
+const ContactItem = styled.li`
+  margin-bottom: 20px;
+`;
+const ContactBtn = styled.button`
+  margin-left: 20px;
+  border-radius: 20px;
+  border: transparent;
+  padding: 10px 25px;
+  background-color: burlywood;
+`;
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,19 +1,25 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
+const onChange = ({ value, onChange }) => {
+  return (
+    <form action="input">
+      <Filter type="text" value={value} onChange={onChange} />
+    </form>
+  );
+};
+export default onChange;
 
-const onChange = ({value, onChange})=>{
+onChange.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
 
-// this.props.onInput( event.currentTarget.value)
-
-
-        return(
-            <form action="input">
-
-                <input type="text" 
-                value={value} onChange ={onChange}
-     />
-            </form>
-        )
-    
-}
-export default onChange
+const Filter = styled.input`
+  width: 300px;
+  padding: 10px 40px;
+  border-radius: 20px;
+  border: transparent;
+  margin-bottom: 30px;
+`;
